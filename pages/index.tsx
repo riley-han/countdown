e54fd42eac04db4d2c56728458c87e33494e8f59
Riley Hansen <rileyhansen29@gmail.com>
2021-06-30 21:39:34 -0600

    styled component set up WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import DateCard from '../src/components/Date/components/DateCard'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home = () => {
-  return(
+  return (
     <div>
       <h2>CountDown</h2>
-      <DateCard/>
     </div>
-  )
-}
-
-
-
-
-
-
+  );
+};
 
 export default Home;
--- src/components/Date/components/DateCard.tsx
+++ src/components/Date/components/DateCard.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { ReactFragment, ReactInstance, ReactText } from 'react';
+import styled from 'styled-components';
+
+const containerDiv: ReactInstance = styled.div`
+  display: flex;
+  width: 200px;
+  height: 200px;
+  background-color: lightblue;
+`;
 
 interface Props {
   minDate: boolean;
   maxDate: boolean;
+  selectedDate: Date;
+  onDateSelect: () => void;
+  
+
 }
-const DateCard: React.FC<Props> = () => {
-  return (
-    <div>
-      <h1>Date Card</h1>
-    </div>
-  );
+const DateCard: React.FC<Props> = ({
+  minDate,
+  maxDate,
+  selectedDate,
+  onDateSelect,
+}) => {
+  return <containerDiv{
+      minDate, 
+      maxDate, 
+      selectedDate, 
+      onDateSelect({onselect})}></containerDiv>;
 };
 
 export default DateCard;
